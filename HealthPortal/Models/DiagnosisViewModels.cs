4307a8f24d8a7d429c0398a5963a8481d3368cff
at870 <austintsai7@gmail.com>
2018-04-03 00:52:20 -0400

    Added Diagnosis pages. Still need to implement Drop down menu for adding diagnosis to patients.

--- HealthPortal/Models/DiagnosisViewModels.cs
+++ HealthPortal/Models/DiagnosisViewModels.cs
@@ -10,5 +10,18 @@ namespace HealthPortal.Models
     public class DiagnosisIndexViewModel
     {
         public virtual IList<Diagnosis> Diagnoses { get; set; }
+        public virtual IList<ApplicationUser> PatientList { get; set; }
+    }
+
+    public class ViewPatientViewModel
+    {
+        public string PatientName { get; set; }
+        public virtual IList<Diagnosis> Diagnoses { get; set; }
+    }
+
+    public class AddDiagnosisViewModel
+    {
+        [Required]
+        public string DiagnosisName { get; set; }
     }
 }
