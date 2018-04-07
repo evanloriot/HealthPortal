2febabcb5d8a660273c5d7a77f211913313593ec
at870 <austintsai7@gmail.com>
2018-04-07 00:41:52 -0400

    Prescriptions. Need to change names and stuff. Also need to change Adding Prescription Page.

--- HealthPortal/Controllers/PrescriptionController.cs
+++ HealthPortal/Controllers/PrescriptionController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using HealthPortal.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,7 +52,208 @@ namespace HealthPortal.Controllers
         // GET: Prescription
         public ActionResult Index()
         {
-            return View();
+            if (User.IsInRole("Patient"))
+            {
+                var userID = User.Identity.GetUserId();
+                var db = new ApplicationDbContext();
+                var dms = db.PrescriptionMap.Where(u => u.UserID == userID).ToList();
+
+                List<Prescriptions> prescriptions = new List<Prescriptions>();
+
+                foreach (var item in dms)
+                {
+                    prescriptions.Add(item.Prescription);
+                }
+
+                var model = new PrescriptionsIndexViewModel
+                {
+                    Prescriptions = prescriptions
+                };
+
+                return View(model);
+            }
+            else if (User.IsInRole("Doctor"))
+            {
+                var docID = User.Identity.GetUserId();
+                var db = new ApplicationDbContext();
+                var docpatients = db.Users.Where(u => u.PrimaryPhysician.Id == docID).ToList();
+
+                var model = new PrescriptionsIndexViewModel
+                {
+                    PatientList = docpatients
+                };
+
+                return View(model);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        public ActionResult ViewPatient(string userID, PrescriptionMessageId? message)
+        {
+            if (userID == null)
+            {
+                return View("Error");
+            }
+
+            ViewBag.StatusMessage = message == PrescriptionMessageId.DeletePrescriptionFromPatientSuccess ? "Prescription successfully removed from patient."
+                : "";
+
+            var db = new ApplicationDbContext();
+            var dms = db.PrescriptionMap.Where(u => u.UserID == userID).ToList();
+
+            var name = db.Users.Where(u => u.Id == userID).FirstOrDefault().Identifier.FullName;
+
+            List<Prescriptions> prescriptions = new List<Prescriptions>();
+            foreach (var item in dms)
+            {
+                prescriptions.Add(item.Prescription);
+            }
+
+            var model = new ViewPatientPrescriptionViewModel
+            {
+                Prescriptions = prescriptions,
+                PatientName = name,
+                PatientID = userID
+            };
+
+            return View(model);
+        }
+
+        public ActionResult ViewPrescriptions(PrescriptionMessageId? message)
+        {
+            ViewBag.StatusMessage = message == PrescriptionMessageId.DeletePrescriptionSuccess ? "Prescription successfully deleted."
+                : message == PrescriptionMessageId.DeletePrescriptionFailure ? "Prescription cannot be deleted as some patients still have this prescription."
+                : "";
+
+            var db = new ApplicationDbContext();
+            var model = new ViewPrescriptionsViewModel
+            {
+                Prescriptions = db.Prescriptions.ToList()
+            };
+            return View(model);
+        }
+
+        public ActionResult DeletePrescription(int? ID)
+        {
+            if (ID == null)
+            {
+                return View("Error");
+            }
+            var db = new ApplicationDbContext();
+            var remove = db.Prescriptions.Where(u => u.PrescriptionID == ID).FirstOrDefault();
+            db.Prescriptions.Remove(remove);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("ViewPrescriptions", new { message = PrescriptionMessageId.DeletePrescriptionFailure });
+            }
+            return RedirectToAction("ViewPrescriptions", new { message = PrescriptionMessageId.DeletePrescriptionSuccess });
+        }
+
+        public ActionResult DeletePrescriptionFromPatient(string userID, int? ID)
+        {
+            if (ID == null || userID == null)
+            {
+                return View("Error");
+            }
+            var db = new ApplicationDbContext();
+            var remove = db.PrescriptionMap.Where(u => u.UserID == userID && u.PrescriptionID == ID).FirstOrDefault();
+            db.PrescriptionMap.Remove(remove);
+            db.SaveChanges();
+            return RedirectToAction("ViewPatient", new { userID, message = PrescriptionMessageId.DeletePrescriptionSuccess });
+        }
+
+        public ActionResult AddPrescription()
+        {
+            var model = new AddPrescriptionViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddPrescription(AddPrescriptionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var db = new ApplicationDbContext();
+
+            var p = new Prescriptions
+            {
+                PrescriptionName = model.PrescriptionName
+            };
+
+            db.Prescriptions.Add(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddPrescriptionToPatient(string patientID)
+        {
+
+            if (patientID == null)
+            {
+                return View("Error");
+            }
+
+            var db = new ApplicationDbContext();
+
+            var non = from map in db.PrescriptionMap
+                      join prescription in db.Prescriptions on map.PrescriptionID equals prescription.PrescriptionID
+                      where map.UserID == patientID
+                      select prescription;
+
+            var p = db.Prescriptions.ToList().Except(non).ToList();
+
+            var name = db.Users.Where(u => u.Id == patientID).FirstOrDefault().Identifier.FullName;
+
+            var model = new AddPrescriptionToPatientViewModel
+            {
+                Prescriptions = p,
+                PatientName = name,
+                PatientID = patientID,
+            };
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddPrescriptionToPatient(AddPrescriptionToPatientViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var db = new ApplicationDbContext();
+
+            var patientPrescription = new PrescriptionsMap
+            {
+                UserID = model.PatientID,
+                PrescriptionID = model.PrescriptionID
+            };
+
+            db.PrescriptionMap.Add(patientPrescription);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public enum PrescriptionMessageId
+        {
+            DeletePrescriptionSuccess,
+            DeletePrescriptionFailure,
+            DeletePrescriptionFromPatientSuccess
         }
     }
 }
---
+++ HealthPortal/Models/PrescriptionsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealthPortal.Models
+{
+    public class PrescriptionsIndexViewModel
+    {
+        public virtual IList<Prescriptions> Prescriptions { get; set; }
+        public virtual IList<ApplicationUser> PatientList { get; set; }
+    }
+
+    public class ViewPatientPrescriptionViewModel
+    {
+        public string PatientName { get; set; }
+        public virtual IList<Prescriptions> Prescriptions { get; set; }
+        public string PatientID { get; set; }
+    }
+
+    public class AddPrescriptionViewModel
+    {
+        [Required]
+        [Display(Name = "Prescription Name")]
+        public string PrescriptionName { get; set; }
+    }
+
+    public class AddPrescriptionToPatientViewModel
+    {
+        public virtual IList<Prescriptions> Prescriptions { get; set; }
+        public int PrescriptionID { get; set; }
+        public string PatientID { get; set; }
+        public string PatientName { get; set; }
+    }
+
+    public class ViewPrescriptionsViewModel
+    {
+        public virtual IList<Prescriptions> Prescriptions { get; set; }
+    }
+}
